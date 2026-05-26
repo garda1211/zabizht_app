1241854c3ac3ad391bdd77b1a9d84878c3c2c24f
garda1211 <v705784@gmail.com>
2026-05-27 01:23:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,39 @@
-const CACHE_NAME = "zabizht-app-v3";
+const CACHE_NAME = "zabizht-cache-v1";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+"./",
+"./index.html",
+"./manifest.json"
 ];
 
-// установка
 self.addEventListener("install", event => {
-  console.log("Service Worker установлен");
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
 });
 
-// запросы
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
 
-// обновление
-self.addEventListener("activate", event => {
-  console.log("Service Worker активирован");
+event.respondWith(
 
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+caches.match(event.request)
+
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
